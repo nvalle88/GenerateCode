89c6441d2bba6b4047457cbe0ac5fbddd4ecc402
Néstor Valle <nestorvalle880416@gmail.com>
2018-09-27 19:32:58 -0500

    Final Versión 1.0

---
+++ GenerateCode/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CodeGenerate;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Example();
+        }
+
+        private static void  Example()
+        {
+            GenerateCode.Lower = 10;
+            GenerateCode.Top = 25;
+            Console.WriteLine(GenerateCode.Generate().ToString());
+
+            Console.WriteLine(GenerateCode.Generate(10,66).ToString());
+            Console.ReadLine();
+            Example();
+        }
+    }
+}
---
+++ GenerateCode/GenerateCode/GenerateCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CodeGenerate
+{
+    public static class GenerateCode
+    {
+        public static int Lower { get; set; }
+        public static int Top { get; set; }
+
+        public static int Generate(int lower, int top)
+        {
+            var rdm = new Random();
+            var number = rdm.Next(lower, top);
+            return number;
+
+        }
+
+        public static int Generate()
+        {
+            var rdm = new Random();
+            var number = rdm.Next(Lower, Top);
+            return number;
+
+        }
+
+    }
+}
